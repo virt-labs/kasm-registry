30a5488abda27e7f6c3c28179b98cc2c03562071
virt-labs <117615520+virt-labs@users.noreply.github.com>
2026-03-22 18:41:27 +0100

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
     name: 'Kasm Technologies',
     description: 'The official store for Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
+    listUrl: 'https://virt-labs.github.io/kasm-registry/',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
